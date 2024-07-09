37b7ec91fab2f4a5805b6c19377a5d872eeda902
Rafael Freitas <oraffaudi@gmail.com>
2024-07-08 21:53:20 -0300

    what are streams

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -10,6 +10,23 @@ export class AboutComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+
+    document.addEventListener('click', event => {
+      console.log(event);
+    });
+
+    let counter = 0;
+
+    setInterval(() => {
+      console.log(counter);
+      counter++;
+    }, 1000);
+
+
+    setTimeout(() =>{
+      console.log("finished");
+    }, 3000)
+
   }
 
 }
