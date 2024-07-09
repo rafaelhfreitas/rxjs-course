647698f403e4965d1de6dc4f4e7c328339a4d81a
Rafael Freitas <oraffaudi@gmail.com>
2024-07-09 20:37:18 -0300

    switchMap operator to do search from a element

--- src/app/course/course.component.ts
+++ src/app/course/course.component.ts
@@ -25,10 +25,10 @@ import { createHttpObservable } from '../common/util';
 })
 export class CourseComponent implements OnInit, AfterViewInit {
 
-
     course$: Observable<Course>;
     lessons$: Observable<Lesson[]>;
 
+    courseId: string;
 
     @ViewChild('searchInput', { static: true, read: ElementRef }) input: ElementRef;
 
@@ -39,34 +39,35 @@ export class CourseComponent implements OnInit, AfterViewInit {
 
     ngOnInit() {
 
-        const courseId = this.route.snapshot.params['id'];
+        this.courseId = this.route.snapshot.params['id'];
 
-        this.course$ = createHttpObservable(`/api/courses/${courseId}`) as Observable<Course>;
+        this.course$ = createHttpObservable(`/api/courses/${this.courseId}`) as Observable<Course>;
 
-        this.lessons$ = createHttpObservable(`/api/lessons?courseId=${courseId}&pageSize=100`)
-                            .pipe(
-                                map(res => res["payload"])
-                            );
-
-
+        
     }
-
+    
     ngAfterViewInit() {
 
-
-        fromEvent<any>(this.input.nativeElement, 'keyup')
+        const searchLessons$ = fromEvent<any>(this.input.nativeElement, 'keyup')
             .pipe(
                 map(event => event.target.value),
                 debounceTime(400),
-                distinctUntilChanged()
+                distinctUntilChanged(),
+                switchMap(search => this.loadLessons(search))
             )
-            .subscribe(console.log);
 
-
-
+        const initialLessons$ = this.loadLessons();
+            
+        this.lessons$ = concat(initialLessons$, searchLessons$);
     }
 
-
+    loadLessons(search = ''): Observable<Lesson[]> {
+        return createHttpObservable(
+                `/api/lessons?courseId=${this.courseId}&pageSize=100&filter=${search}`,)
+                .pipe(
+                    map(res => res["payload"])
+                )
+    }
 
 
 }
