dfc0f3f6614cb325f9527aa061446607db93de20
Rafael Freitas <oraffaudi@gmail.com>
2024-07-08 23:25:55 -0300

    component with rxjs - reactive design

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -13,25 +13,27 @@ import { createHttpObservable } from '../common/util';
 export class HomeComponent implements OnInit {
 
 
-    beginnerCourses: Course[];
-    advancedCourses: Course[];
+    beginnerCourses$: Observable<Course[]>;
+    advancedCourses$: Observable<Course[]>;
 
     ngOnInit() {
         const http$ = createHttpObservable('/api/courses');
 
         const courses$: Observable<Course[]> = http$
         .pipe(
-            map(res => Object.values(res["payload"]))
+            map(res => Object.values<Course>(res["payload"]))
         )
-    
-        courses$.subscribe(
-          courses => {
-            this.beginnerCourses = courses.filter(course => course.category == 'BEGINNER');
-            this.advancedCourses = courses.filter(course => course.category == 'ADVANCED');
-          },
-          noop,
-          () => console.log('completed')
-        );
+
+
+        this.beginnerCourses$ = courses$
+            .pipe(
+                map(courses => courses.filter(course => course.category == 'BEGINNER'))
+            );
+        
+        this.advancedCourses$ = courses$
+            .pipe(
+                map(courses => courses.filter(course => course.category == 'ADVANCED'))
+            );
 
 
     }
