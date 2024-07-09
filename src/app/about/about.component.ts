13484388fedc4569c82ce9319fedd0ed9ea315b6
Rafael Freitas <oraffaudi@gmail.com>
2024-07-09 15:59:23 -0300

    concat operator to concat observables

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
-import { fromEvent, interval, noop, Observable, timer } from 'rxjs';
+import { concat, fromEvent, interval, noop, Observable, of, timer } from 'rxjs';
 import { createHttpObservable } from '../common/util';
 import { map } from 'rxjs/operators';
 
@@ -14,7 +14,14 @@ export class AboutComponent implements OnInit {
 
   ngOnInit() {
     
+      const source1$ = of(1, 2, 3);
+      const source2$ = of(4, 5, 6);
+      const source3$ = of(7, 8, 9);
 
+      const result$ = concat(source1$, source2$, source3$);
+
+      result$.subscribe(console.log);
+      
 
   }
 
