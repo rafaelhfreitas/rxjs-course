783424a8fa161a4931f8cb55ce77fb55d2bf9fb2
Rafael Freitas <oraffaudi@gmail.com>
2024-07-08 23:10:12 -0300

    component with rxjs - imperative design

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,7 +1,8 @@
 import {Component, OnInit} from '@angular/core';
 import {Course} from "../model/course";
-import {interval, Observable, of, timer} from 'rxjs';
+import {interval, noop, Observable, of, timer} from 'rxjs';
 import {catchError, delayWhen, map, retryWhen, shareReplay, tap} from 'rxjs/operators';
+import { createHttpObservable } from '../common/util';
 
 
 @Component({
@@ -12,12 +13,25 @@ import {catchError, delayWhen, map, retryWhen, shareReplay, tap} from 'rxjs/oper
 export class HomeComponent implements OnInit {
 
 
-    constructor() {
-
-    }
+    beginnerCourses: Course[];
+    advancedCourses: Course[];
 
     ngOnInit() {
+        const http$ = createHttpObservable('/api/courses');
 
+        const courses$: Observable<Course[]> = http$
+        .pipe(
+            map(res => Object.values(res["payload"]))
+        )
+    
+        courses$.subscribe(
+          courses => {
+            this.beginnerCourses = courses.filter(course => course.category == 'BEGINNER');
+            this.advancedCourses = courses.filter(course => course.category == 'ADVANCED');
+          },
+          noop,
+          () => console.log('completed')
+        );
 
 
     }
