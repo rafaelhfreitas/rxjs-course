6bcd7cc4007a6169d04ed0f87f571865c3ea12d0
Rafael Freitas <oraffaudi@gmail.com>
2024-07-10 08:42:10 -0300

    startWith operator to do a search with a content before does the request

--- src/app/course/course.component.ts
+++ src/app/course/course.component.ts
@@ -48,17 +48,15 @@ export class CourseComponent implements OnInit, AfterViewInit {
     
     ngAfterViewInit() {
 
-        const searchLessons$ = fromEvent<any>(this.input.nativeElement, 'keyup')
+        this.lessons$ = fromEvent<any>(this.input.nativeElement, 'keyup')
             .pipe(
                 map(event => event.target.value),
+                startWith(''),
                 debounceTime(400),
                 distinctUntilChanged(),
                 switchMap(search => this.loadLessons(search))
             )
-
-        const initialLessons$ = this.loadLessons();
             
-        this.lessons$ = concat(initialLessons$, searchLessons$);
     }
 
     loadLessons(search = ''): Observable<Lesson[]> {
