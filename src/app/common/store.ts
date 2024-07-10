cfbdc657be8242697b3e66b5e8474ea25ba0154e
Rafael Freitas <oraffaudi@gmail.com>
2024-07-10 16:05:19 -0300

    the store pattern - loading initial data, selector methods, consuming data

--- src/app/common/store.ts
+++ src/app/common/store.ts
@@ -1,6 +1,8 @@
 import {  Injectable } from "@angular/core";
-import { BehaviorSubject, Observable, Subject } from "rxjs";
+import { BehaviorSubject, Observable, Subject, timer } from "rxjs";
 import { Course } from "../model/course";
+import { tap, map, shareReplay, retryWhen, delayWhen } from "rxjs/operators";
+import { createHttpObservable } from "./util";
 
 @Injectable({
     providedIn: 'root'
@@ -9,7 +11,36 @@ export class Store {
 
 
     private subject = new BehaviorSubject<Course[]>([]);
-
     courses$: Observable<Course[]> = this.subject.asObservable();
+
+    init() {
+        const http$ = createHttpObservable('/api/courses');
+
+        const courses$: Observable<Course[]> = http$
+            .pipe(
+                tap(() => console.log("HTTP request executed")),
+                map(res => Object.values(res["payload"]))
+            )
+            .subscribe(courses => 
+                this.subject.next(courses)
+            );
+
+    }
+
+
+    filterByCategory(category): Observable<Course[]> {
+        return this.courses$
+            .pipe(
+                map(courses => courses
+                    .filter(course => course.category == category))
+            );
+    }
+
+
+    selectBeginnerCourses(): Observable<Course[]> {
+        return this.filterByCategory('BEGINNER');
+
+    }
+
 
 }
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -25,31 +25,13 @@ export class HomeComponent implements OnInit {
 
     ngOnInit() {
 
-        const http$ = createHttpObservable('/api/courses');
 
-        const courses$: Observable<Course[]> = http$
-            .pipe(
-                tap(() => console.log("HTTP request executed")),
-                map(res => Object.values(res["payload"]) ),
-                shareReplay(),
-                retryWhen(errors =>
-                    errors.pipe(
-                    delayWhen(() => timer(2000)
-                    )
-                ) )
-            );
+        const courses$ = this.store.courses$;
 
-        this.beginnerCourses$ = courses$
-            .pipe(
-                map(courses => courses
-                    .filter(course => course.category == 'BEGINNER'))
-            );
+        this.beginnerCourses$ = this.store.selectBeginnerCourses();
+        //this.beginnerCourses$ = this.store.filterByCategory('BEGINNER');
 
-        this.advancedCourses$ = courses$
-            .pipe(
-                map(courses => courses
-                    .filter(course => course.category == 'ADVANCED'))
-            );
+        this.advancedCourses$ = this.store.filterByCategory('ADVANCED');
 
     }
 
