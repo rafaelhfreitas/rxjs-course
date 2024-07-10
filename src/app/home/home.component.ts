1515d9f6f3cfa044ab106b84e85fccde1050c481
Rafael Freitas <oraffaudi@gmail.com>
2024-07-10 07:54:07 -0300

    errorHandling with catchError operator used to chose another action like call another observable to provide auxiliar stream of data to componente

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -23,7 +23,16 @@ export class HomeComponent implements OnInit {
         .pipe(
             tap(() => console.log('HTTP request executed')),
             map(res => Object.values<Course>(res["payload"])),
-            shareReplay()
+            shareReplay(),
+            catchError(error => of([{
+                id: 0,
+                description: "RxJs In Practice Course",
+                iconUrl: 'https://s3-us-west-1.amazonaws.com/angular-university/course-images/rxjs-in-practice-course.png',
+                courseListIcon: 'https://angular-academy.s3.amazonaws.com/main-logo/main-page-logo-small-hat.png',
+                longDescription: "Understand the RxJs Observable pattern, learn the RxJs Operators via practical examples",
+                category: 'BEGINNER',
+                lessonsCount: 10
+            }]))
         )
 
 
